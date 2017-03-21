58e1d684a873db3b5d3cd859dfee8563055d5e8c
sashajustice <sabrinnasreddin@gmail.com>
2017-03-21 11:52:24 -0700

    add few routes and tests

--- sandbox-server/server.js
+++ sandbox-server/server.js
@@ -13,6 +13,27 @@ server.get('/', function(request, response) {
   response.status(200).send('Welcome to Sandbox!')
 })
 
+server.get('/search', function (request, response) {
+  response.set('Content-type', 'text/plain')
+  response.status(200).send("doodads")
+})
+
+server.get('/things', function(request, response) {
+  response.set('Content-type', 'text/plain')
+  response.status(400).send("You didn't provide a search query term :(" {
+})
+
+server.post('/things', function(request, response) {
+  response.set('Content-type', 'text/plain')
+  response.status(201).send("flying car!") {
+  })
+
+
+
+
+
+
+
 server.set(port)
 
 http.listen(port)
