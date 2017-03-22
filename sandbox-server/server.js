10abe7ece880ff654ae38ed2d60452aa1d391cbb
yaseenagag <yaseen.uc@gmail.com>
2017-03-21 17:45:39 -0700

    implement rest of routes and pass all tests

--- sandbox-server/server.js
+++ sandbox-server/server.js
@@ -15,36 +15,56 @@ server.get('/', function(request, response) {
 
 server.get('/search', function (request, response) {
   response.set('Content-type', 'text/plain')
-  response.status(200).send("doodads")
-})
-
-server.get('/things', function(request, response) {
-  response.set('Content-type', 'text/plain')
-  response.status(400).send("You didn't provide a search query term :(" {
+  if ('q' in request.query) {
+    response.status(200).send("You searched for:'doodads'")
+  } else {
+    response.status(400).send("You didn't provide a search query term :(")
+  }
 })
 
 server.post('/things', function(request, response) {
   response.set('Content-type', 'text/plain')
-  response.status(201).send("flying car!") {
+  response.status(201).send(`New thing created: flying car!`)
   })
 
-server.get('/search', functions(request, response) {
-  response.set('Content-type', 'text-plain')
-  response.status(200).send("This is a plain text file")
+server.get('/somefile', function(request, response) {
+  if (request.accepts('text/html')) {
+    response.set('Content-type', 'text/html')
+    response.status(200).send('<!DOCTYPE html><html><body>This is an HTML file</body></html>')
+  } else {
+    response.status(400).end()
+  }
 })
 
-server.get('/things', functions(request, response) {
-  response.set('Content-type, text-plain')
-  response.status()
-}
+server.get('/myjsondata', function(request, response) {
+  if(request.accepts('application/json')) {
+    response.set('Content-type', 'application/json')
+    response.status(200).send({ 'title': 'some JSON data' })
+  } else {
+    response.status(400).end()
+  }
+})
 
+server.get('/old-page', function(request, response) {
+  response.redirect(301,'/newpage')
+})
 
+server.get('/newpage', function(request, response) {
+  response.status(301)
+  response.set('content-location','http://localhost:3000/newpage')
+  .send()
+})
 
+server.post('/admin-only', function(request, response) {
+  response.status(403).end()
+})
 
+server.get('/not-a-page', function(request, response) {
+  response.status(404).end()
+})
 
-
-server.set(port)
-
-http.listen(port)
+server.get('/server-error', function(request, response) {
+  response.status(500).end()
+})
 
 module.exports = server
